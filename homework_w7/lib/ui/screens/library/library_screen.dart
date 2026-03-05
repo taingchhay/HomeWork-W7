9b97d6126bddd90ca09a8b6fa88efb40cfdee669
taingchhay <filmakame@gmail.com>
2026-03-05 08:53:34 +0700

    remove class  Songtile from library screen

--- homework_w7/lib/ui/screens/library/library_screen.dart
+++ homework_w7/lib/ui/screens/library/library_screen.dart
@@ -24,28 +24,3 @@ class LibraryScreen extends StatelessWidget {
     );
   }
 }
-
-class SongTile extends StatelessWidget {
-  const SongTile({
-    super.key,
-    required this.song,
-    required this.isPlaying,
-    required this.onTap,
-  });
-
-  final Song song;
-  final bool isPlaying;
-  final VoidCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: onTap,
-      title: Text(song.title),
-      trailing: Text(
-        isPlaying ? "Playing" : "",
-        style: TextStyle(color: Colors.amber),
-      ),
-    );
-  }
-}
